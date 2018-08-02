8424bbabea7f13be40c92d529cdfc01ffdd3664f
aboud20-meet <aboud20@meet.mit.edu>
2018-08-02 17:24:19 +0300

    dkjodjeokejhcgfghjk

--- event-listeners/event-listeners.js
+++ event-listeners/event-listeners.js
@@ -1,6 +1,8 @@
 // YOUR TASK: Add more pictures!
-var pictures = ['./imgs/dog.jpg'];
+var pictures = ['./imgs/pic1.jpeg','./imgs/pic2.jpeg','./imgs/pic3.jpeg','./imgs/pic4.jpeg','./imgs/pic5.jpeg'];
 var currentIndex = 0;
+//var PicList = document.getElementByTagName("img");
+
 
 function showNextPicture() {
   currentIndex++; // increment current picture
@@ -9,5 +11,11 @@ function showNextPicture() {
     currentIndex = 0;
   }
 
+  document.getElementsByTagName("img")[0].addEventListener("click", function (event) {
+  	alert("hiiii");
+   
+});
+
   // YOUR TASK: Finish this function!
 }
+showNextPicture();
--- html-events/html-events.js
+++ html-events/html-events.js
@@ -1,4 +1,7 @@
 // Put all your JavaScript in this file!
-function alertMe() {
+function alertMe(object) {
     var color=prompt("CHOSE A COLOR");
+    object.style.backgroundColor = color;
+    var b=document.getElementsByTagName("body");
+    b[0].style.backgroundColor = color;
 }
